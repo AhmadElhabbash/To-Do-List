643ac7f222afbd02153cbf4e1eff82c67a2cfa7b
Ahmed Elhabbash <ahmedelhabbash1@gmail.com>
2025-07-04 17:40:19 +0300

    fixed update ui data

--- src/tasks.js
+++ src/tasks.js
@@ -1,23 +1,28 @@
 import { saveTasks, loadTasks } from "./storage.js";
+import { renderTasks } from "./ui.js";
 
 export let tasks = loadTasks();
 
 export function addTask(text) {
   tasks.push({ text, done: false });
   saveTasks(tasks);
+  renderTasks();
 }
 
 export function deleteTask(index) {
   tasks.splice(index, 1);
   saveTasks(tasks);
+  renderTasks();
 }
 
 export function toggleDone(index) {
   tasks[index].done = !tasks[index].done;
   saveTasks(tasks);
+  renderTasks();
 }
 
 export function editTask(index, newText) {
   tasks[index].text = newText;
   saveTasks(tasks);
+  renderTasks();
 }
